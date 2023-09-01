715216f988dd6802f77cfa2b40455bbd2098b3ff
Junlys3 <125740363+Junlys3@users.noreply.github.com>
2023-09-01 16:15:31 -0300

    Ex

---
+++ aula011/ex008.9.js
@@ -0,0 +1,17 @@
+
+var vel = 40
+console.log(`A sua velocidade é ${vel} Km/h`)
+if (vel > 40){
+    console.log('Você foi MULTADO')
+}
+
+var país = 'EUA'
+
+if (país == 'Brasil') {
+    console.log('Tu é brasileiro carai')
+ } else {
+        console.log('Gringo')
+    }
+
+
+
